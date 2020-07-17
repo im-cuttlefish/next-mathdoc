bfe41e4d499f1a8648b727e3fa89474a2cf4e8ea
im-cuttlefish <42869736+im-cuttlefish@users.noreply.github.com>
2020-07-17 13:12:18 +0900

    enable primitive CSS and remove unused dependencies

--- src/util/mergeThemes.ts
+++ src/util/mergeThemes.ts
@@ -1,9 +1,39 @@
 import { Theme } from "../types";
 
+const initialTheme: Theme = {
+  // ref
+  refLink: "mathdoc-ref-link",
+  refInternalLink: "mathdoc-ref-internal-link",
+  refExternalLink: "mathdoc-ref-external-link",
+
+  //theorem
+  theoremContainer: "mathdoc-theorem-container",
+  theoremContent: "mathdoc-theorem-content",
+  theoremTitle: "mathdoc-theorem-title",
+
+  // proof
+  proofContainer: "mathdoc-proof-container",
+  proofContent: "mathdoc-proof-content",
+  proofStartMark: "mathdoc-proof-start-mark",
+  proofEndMark: "mathdoc-proof-end-mark",
+
+  // question
+  questionContainer: "mathdoc-question-container",
+  questionContent: "mathdoc-question-content",
+  questionTitle: "mathdoc-question-title",
+
+  // answer
+  answerContainer: "mathdoc-answer-container",
+  answerContent: "mathdoc-answer-content",
+  answerTitle: "mathdoc-answer-title",
+};
+
 export const mergeThemes = (theme: Theme | Theme[]) => {
   if (!Array.isArray(theme)) {
-    return theme;
+    theme = [theme];
   }
+
+  theme = [...theme, initialTheme];
 
   return theme.reduce((prev, current) => {
     const merged = { ...prev };
